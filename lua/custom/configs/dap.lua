558d9393b0cd68b3e1da5fc09c6fa12c9450e6f4
mjolk <2mjolk@gmail.com>
2023-08-05 12:03:03 +0200

    dap mappings, dap-ui, dap-hover, lsp cfgs, etc

---
+++ lua/custom/configs/dap.lua
@@ -0,0 +1,22 @@
+local dap = require "dap"
+
+dap.adapters.cppdbg = {
+  id = "cppdbg",
+  type = "executable",
+  command = "OpenDebugAD7", -- add ~/.local/share/nvim/mason/bin to PATH!
+}
+
+dap.configurations.cpp = {
+  {
+    name = "Launch file",
+    type = "cppdbg",
+    request = "launch",
+    program = function()
+      return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+    end,
+
+    cwd = "${workspaceFolder}",
+    stopAtEntry = true,
+  },
+}
+dap.configurations.c = dap.configurations.cpp
