80965f7fc5818445b01406bae8f34b8c10f0cd99
mjolk <2mjolk@gmail.com>
2023-08-14 10:05:18 +0200

    correct use of dependencies w Lazy, sane dap mappings, working dap config

--- lua/custom/configs/header.lua
+++ lua/custom/configs/header.lua
@@ -2,20 +2,16 @@ local header = require "header"
 header.setup {
   length = 80, -- headers of different sizes are incompatible with each other
   margin = 5,
-  default_map = true, -- default Mapping <F1> in normal mode
+  default_map = false, -- default Mapping <F1> in normal mode
   auto_update = true, -- update header when saving
   asciiart = {
-    [[
-.........../´¯¯/)
-.........,/¯.../
-........./..../
-...../´¯/'..'/´¯¯`·¸
-./'/.../..../....../¨¯\
-('(....´...´... ¯~/'..')
-.\..............'...../
-..\....\.........._.·´
-...\..............(
-....\..............\
-]],
+    ".........../´¯¯/).........",
+    ".........,/¯.../..........",
+    "........./..../...........",
+    "...../´¯/'..'/´¯¯`·¸......",
+    "./'/.../..../....../¨¯....",
+    "('(....´...´... ¯~/'..')..",
+    ".\\..............'...../..",
+    "..\\....\\.........._.·´..",
   }, -- headers with different ascii arts are incompatible with each other
 }
